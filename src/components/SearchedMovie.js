33d7b22b50c8a07f60d3a786d9411a5993b597fc
Shesh <76209616+shesh2509@users.noreply.github.com>
2024-06-01 19:34:49 +0530

    Update SearchedMovie.js

--- src/components/SearchedMovie.js
+++ src/components/SearchedMovie.js
@@ -159,6 +159,75 @@ const Icon = styled.div`
     cursor: pointer;
 `
 
+const Popup2 = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    width: 60vw;
+    height: 60vh;
+    transform: translate(-50%, -50%);
+    background: #212121;
+    color: black;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+    z-index: 1000;
+`
+
+const Overlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 999;
+`
+
+const Full = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const Div2 = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+const Div3 = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`
+const Title = styled.h2`
+  color: white;
+`
+const Para = styled.p`
+  color: white;
+  margin-top: 0;
+`
+
+const PosterDiv = styled.div`
+`
+
+const Wrapper1 = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+
+const Image1 = styled.img`
+    height: 150px;
+    width: 150px;
+    border-radius: 50%;
+`
 
 
 function Popup({ lists, onClose, onAddToList, accessToken, movie }) {
@@ -258,6 +327,10 @@ function Popup({ lists, onClose, onAddToList, accessToken, movie }) {
 export default function SearchedMovie({movie, accessToken}) {
   const [showPopup, setShowPopup] = useState(false);
   const [lists, setLists] = useState([]);
+  const [showPopDetail, setShowPopDetail] = useState(false);
+  const [movieDetails, setMovieDetails] = useState(null);
+
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -272,7 +345,18 @@ export default function SearchedMovie({movie, accessToken}) {
     fetchData();
   }, [accessToken]);
 
+  const apiKey = process.env.REACT_APP_OMDB_API_KEY;
+  if (!apiKey) {
+    console.error("OMDB API key is missing.");
+    return;
+}
 
+  const fetchMovieDetails = async () => {
+  const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`);
+  const data = await response.json();
+  setMovieDetails(data);
+  setShowPopDetail(true);
+  };
 
   const handleAddToList = () => {
     setShowPopup(true);
@@ -307,7 +391,7 @@ const handleAddToListSelection = async (listId) => {
   return (
     <Container>
       <Wrapper>
-        <Div1>
+        <Div1 onClick={fetchMovieDetails}>
           <Image src={movie.Poster} />
           <Ddiv>
             <Name1>{movie.Title.toUpperCase()} <br /></Name1>
@@ -329,6 +413,49 @@ const handleAddToListSelection = async (listId) => {
         movie={movie}
         handleCreateNewList={handleCreateNewList}
         />
+      )}
+
+      {showPopDetail && (
+        <>
+          <Overlay onClick={() => setShowPopup(false)} />
+          <Popup2>
+            {movieDetails ? (
+                <>
+                <Full>
+                <Icon>
+                  <CloseIcon onClick={() => setShowPopDetail(false)}/>
+                </Icon>
+                <Title>{movieDetails.Title}</Title>
+                <Wrapper1>
+                <Div2>
+                    <Para><strong>Year:</strong> {movieDetails.Year}</Para>
+                    <Para><strong>Rated:</strong> {movieDetails.Rated}</Para>
+                    <Para><strong>Released:</strong> {movieDetails.Released}</Para>
+                    <Para><strong>Runtime:</strong> {movieDetails.Runtime}</Para>
+                    <Para><strong>Genre:</strong> {movieDetails.Genre}</Para>
+                    <Para><strong>Writer:</strong> {movieDetails.Writer}</Para>
+                    <Para><strong>Director:</strong> {movieDetails.Director}</Para>
+                    <Para><strong>IMDB Rating:</strong> {movieDetails.imdbRating}</Para>
+                    <Para><strong>BoxOffice:</strong> {movieDetails.BoxOffice}</Para>
+                  </Div2>
+                  <Div3>
+                    <Para><strong>Actors:</strong> {movieDetails.Actors}</Para>
+                    <Para><strong>Plot:</strong> {movieDetails.Plot}</Para>
+                    <Para><strong>Language:</strong> {movieDetails.Language}</Para>
+                    <Para><strong>Country:</strong> {movieDetails.Country}</Para>
+                    <Para><strong>Awards:</strong> {movieDetails.Awards}</Para>
+                  </Div3>
+                  <PosterDiv>
+                    <Image1 src={movie.Poster} />
+                  </PosterDiv>
+                </Wrapper1>
+                </Full>
+                </>
+                ) : (
+                  <p>Loading...</p>
+                )}
+                </Popup2>
+              </>
       )}
     </Container>
     )
